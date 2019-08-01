95d6c967f2a7f6d6ca99108be4dd4602072b7531
ayushgw <ayushgoswami@zoho.com>
2019-08-01 16:27:58 +0530

    linked up form with backend

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,29 @@
 console.log('Client side js is loaded!');
 
-const submitForm = (address) => {
-    fetch('http://localhost:3000/weather?address=' + address)
-        .then((response) => {
-            response.json().then((data) => {
-                if (data.error) return console.log(data.error)
-
-                console.log('Location: ', data.location);
-                console.log('Forecast: ', data.forecast);
-            })
-        })
-}
-
-const weatherForm = document.querySelector('form');
+const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const errorMsg = document.querySelector('#error')
+const result = document.querySelector('#result')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const address = search.value
-    submitForm(address)
+
+    errorMsg.textContent = ''
+    result.textContent = 'Loading...'
+
+    fetch('http://localhost:3000/weather?address=' + address)
+        .then((response) => {
+            response.json().then((data) => {
+                if (data.error) {
+                    errorMsg.textContent = data.error
+                    result.textContent = ''
+                }
+                else {
+                    errorMsg.textContent = ''
+                    result.innerHTML = `Location: ${data.location} <br><br> Forecast: ${data.forecast}`
+                }
+            })
+        })
 })
