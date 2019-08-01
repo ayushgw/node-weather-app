9dec39a8f5f9ff711feb95f4eba16974e32bbaa8
ayushgw <ayushgoswami@zoho.com>
2019-08-01 15:40:02 +0530

    hooked up client js with fetch api

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,23 @@
 console.log('Client side js is loaded!');
+
+const submitForm = (address) => {
+    fetch('http://localhost:3000/weather?address=' + address)
+        .then((response) => {
+            response.json().then((data) => {
+                if (data.error) return console.log(data.error)
+
+                console.log('Location: ', data.location);
+                console.log('Forecast: ', data.forecast);
+            })
+        })
+}
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const address = search.value
+    submitForm(address)
+})
