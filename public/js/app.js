c06428ab65e90d987ce7d0d749e138b2c3d03081
ayushgw <ayushgoswami@zoho.com>
2019-08-01 23:22:08 +0530

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
     errorMsg.textContent = ''
     result.textContent = 'Loading...'
 
-    fetch('http://localhost:3000/weather?address=' + address)
+    fetch('/weather?address=' + address)
         .then((response) => {
             response.json().then((data) => {
                 if (data.error) {
--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast')
 const geocode = require('./utils/geocode')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -80,6 +81,6 @@ app.get('*', (req, res) => {
     res.send('My 404 Page')
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log('Server is up on port' + port);
 })
